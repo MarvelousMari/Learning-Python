4ce593725a27f4b6d73f4dc393049e80dda60dd4
MarvelousMari <mariella@mariellapage.com>
2020-06-12 15:23:57 -0500

    demo of Lists
    demo of lists

---
+++ Lists.py
@@ -0,0 +1,6 @@
+demoList = []
+print("This is an empyt list", demoList)
+demoList.append(1)
+print("this is a list with an item", demoList)
+demoList.append("a Sting")
+print("this is a list with two items", demoList)
