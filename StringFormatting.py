69c280f8ce0fc2b529b957b01fe2bcf4190c8ac4
MarvelousMari <mariella@mariellapage.com>
2020-06-12 18:11:11 -0500

    String Formatting Demo

---
+++ StringFormatting.py
@@ -0,0 +1,6 @@
+name = "Mariella"
+print("Hello, %s!" % name)
+age = 22
+print("%s, you are %d years old." % (name,age))
+favNumbers = [42, 420, 1312,]
+print("A list of your favorite numbers: %s" %favNumbers)
