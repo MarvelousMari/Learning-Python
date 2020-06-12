f4444ecf08042a754b07bcdc3ec6fb993948ed6b
MarvelousMari <mariella@mariellapage.com>
2020-06-12 15:16:55 -0500

    Variables and Types
    demo of some variables and types

---
+++ VariablesAndTypes.py
@@ -0,0 +1,10 @@
+demoInt = 42
+print("this is a demo int ", demoInt)
+demoFloat = 42.0
+print("this is a demo float", demoFloat)
+demoString = "this is a demoString"
+print("this is a demo string ", demoString)
+one = 1
+two = 2
+three = one + two
+print("the variable 'one' = ", one, " the variable 'two' = ", two, " the variable 'three' is declated 'three = one + two' the variable 'three' = ", three )
