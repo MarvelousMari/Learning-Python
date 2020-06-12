4c0b9b3f5e7a23187b4c7966cab58f63f1ebad47
MarvelousMari <mariella@mariellapage.com>
2020-06-12 17:06:43 -0500

    demo of basic operators

---
+++ BasicOperators.py
@@ -0,0 +1,19 @@
+number = 42 + 420 / 1312
+print(number)
+remainder = 13 % 7
+print("the modulo/remainder of 13 % 7 = ", remainder)
+cubed = 2**3
+print("2**3 means 2 cubed the X**Y syntax means X to they Y which in this case = ", cubed)
+helloworld = "hello" + " " + "world"
+print(helloworld, " this was made with this declaration 'helloworld = 'hello' + ' ' +'world''")
+hix3 = "hi " * 3
+print(hix3, " this was made with 'hix3 = 'hi ' * 3' ")
+evenNumbers = [10,12,14,16,18]
+oddNumbers = [11,13,15,17,19]
+allNumbers = oddNumbers + evenNumbers
+print("even numbers list: ", evenNumbers)
+print("odd numbers list: ",oddNumbers)
+print("allNumbers = oddNumbers + evenNumbers")
+print("allNumbers list: ", allNumbers)
+print([1,2,3]*3)
+print("above was made with '[1,2,3]*3'")
